03eaf2a6f0f6b4652a0a49570d39be49da243802
Jan Steggemann <jan.steggemann@cern.ch>
2014-10-09 17:19:00 +0200

    Update mmt

--- mumutau/make_plots.py
+++ mumutau/make_plots.py
@@ -1,50 +1,107 @@
 import sys
 sys.path.append("../Common")
 
+import ROOT
+
 from Plotter import Plotter
-from samples import sampleDict
 
 if __name__ == '__main__':
     variables = [
-    {'var':'BDTG', 'varname':'BDTG', 'legend':'top left', 'logy':False, 'title':'BDT score', 'unit':'', 'nbinsx':10, 'xmin':-1., 'xmax':1.0001, 'save':True},
-    {'var':'Fisher', 'varname':'Fisher', 'legend':'top left', 'logy':False, 'title':'Fisher score', 'unit':'', 'nbinsx':10, 'xmin':-1., 'xmax':1.0001, 'save':True},
+    {'var':'BDTG', 'varname':'BDTG', 'legend':'top left', 'logy':False, 'title':'BDT score', 'unit':'', 'nbinsx':5, 'xmin':-0.8, 'xmax':0.8, 'save':True},
+    {'var':'Fisher', 'varname':'Fisher', 'legend':'top left', 'logy':False, 'title':'Fisher score', 'unit':'', 'nbinsx':5, 'xmin':-0.6, 'xmax':0.6, 'save':True},
     {'var':'bdt_evt_missing_et', 'varname':'bdt_evt_missing_et', 'legend':'top right', 'logy':False, 'title':'E_{T}^{miss}', 'unit':'GeV', 'nbinsx':20, 'xmin':0., 'xmax':300.},
     {'var':'bdt_evt_centrality', 'varname':'bdt_evt_centrality', 'legend':'top right', 'logy':False, 'title':'Centrality', 'unit':'', 'nbinsx':20, 'xmin':0., 'xmax':1.},
-    # {'var':'bdt_evt_sphericity', 'varname':'bdt_evt_sphericity', 'legend':'top right', 'logy':False, 'title':'Sphericity', 'unit':'', 'nbinsx':12, 'xmin':0., 'xmax':1.},
-    # {'var':'bdt_evt_aplanarity', 'varname':'bdt_evt_aplanarity', 'legend':'top right', 'logy':False, 'title':'Aplanarity', 'unit':'', 'nbinsx':12, 'xmin':0., 'xmax':0.4},
+    {'var':'bdt_evt_sphericity', 'varname':'bdt_evt_sphericity', 'legend':'top right', 'logy':False, 'title':'Sphericity', 'unit':'', 'nbinsx':12, 'xmin':0., 'xmax':1.},
+    {'var':'bdt_evt_aplanarity', 'varname':'bdt_evt_aplanarity', 'legend':'top right', 'logy':False, 'title':'Aplanarity', 'unit':'', 'nbinsx':12, 'xmin':0., 'xmax':0.4},
     {'var':'bdt_tau_decaymode', 'varname':'bdt_tau_decaymode', 'legend':'top right', 'logy':False, 'title':'#tau decay mode', 'unit':'', 'nbinsx':11, 'xmin':-0.5, 'xmax':10.5},
-    {'var':'bdt_tau_mass', 'varname':'bdt_tau_mass', 'legend':'top right', 'logy':False, 'title':'#tau_{1} mass', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':3.5},
-    {'var':'bdt_stau_mass', 'varname':'bdt_stau_mass', 'legend':'top right', 'logy':False, 'title':'#tau_{2} mass', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':3.5},
-    {'var':'bdt_tau_isolation', 'varname':'bdt_tau_isolation', 'legend':'top right', 'logy':False, 'title':'#tau_{1} isolation (GeV)', 'unit':'GeV', 'nbinsx':11, 'xmin':0., 'xmax':2.2},
-    {'var':'bdt_stau_isolation', 'varname':'bdt_stau_isolation', 'legend':'top right', 'logy':False, 'title':'#tau_{2} isolation (GeV)', 'unit':'GeV', 'nbinsx':11, 'xmin':0., 'xmax':2.2},
+    {'var':'bdt_tau_mass', 'varname':'bdt_tau_mass', 'legend':'top right', 'logy':False, 'title':'#tau mass', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':3.5},
     {'var':'bdt_evt_leading_btag_pt', 'varname':'bdt_evt_leading_btag_pt', 'legend':'top right', 'logy':False, 'title':'p_{T} b jet_{1}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':200.},
     {'var':'bdt_evt_sleading_btag_pt', 'varname':'bdt_evt_sleading_btag_pt', 'legend':'top right', 'logy':False, 'title':'p_{T} b jet_{2}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':200.},
     {'var':'bdt_evt_leading_btag', 'varname':'bdt_evt_leading_btag', 'legend':'top right', 'logy':False, 'title':'CSV b jet_{1}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
     {'var':'bdt_evt_sleading_btag', 'varname':'bdt_evt_sleading_btag', 'legend':'top right', 'logy':False, 'title':'CSV b jet_{2}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
-    # {'var':'bdt_evt_leading_nbtag', 'varname':'bdt_evt_leading_nbtag', 'legend':'top right', 'logy':False, 'title':'CSV l jet_{1}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
-    # {'var':'bdt_evt_sleading_nbtag', 'varname':'bdt_evt_sleading_nbtag', 'legend':'top right', 'logy':False, 'title':'CSV l jet_{2}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
+    {'var':'bdt_evt_leading_nbtag', 'varname':'bdt_evt_leading_nbtag', 'legend':'top right', 'logy':False, 'title':'CSV l jet_{1}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
+    {'var':'bdt_evt_sleading_nbtag', 'varname':'bdt_evt_sleading_nbtag', 'legend':'top right', 'logy':False, 'title':'CSV l jet_{2}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.},
     {'var':'bdt_evt_max_jet_eta', 'varname':'bdt_evt_max_jet_eta', 'legend':'top right', 'logy':False, 'title':'max(jet |#eta|)', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':5.},
     {'var':'(bdt_evt_max_jet_eta30*(bdt_evt_max_jet_eta30>0.))', 'varname':'bdt_evt_max_jet_eta30', 'legend':'top right', 'logy':False, 'title':'max(jet |#eta|, 30 GeV)', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':5.},
     {'var':'bdt_evt_njet_or', 'varname':'bdt_evt_njet_or', 'legend':'top right', 'logy':False, 'title':'N_{jets} (20 GeV)', 'unit':'', 'nbinsx':10, 'xmin':-0.5, 'xmax':9.5},
     {'var':'bdt_evt_njet_or30', 'varname':'bdt_evt_njet_or30', 'legend':'top right', 'logy':False, 'title':'N_{jets} (30 GeV)', 'unit':'', 'nbinsx':10, 'xmin':-0.5, 'xmax':9.5},
     {'var':'bdt_evt_sumjetpt', 'varname':'bdt_evt_sumjetpt', 'legend':'top right', 'logy':False, 'title':'#Sigma jet p{T}', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':500.},
+    {'var':'bdt_evt_Met', 'varname':'bdt_evt_Met', 'legend':'top right', 'logy':False, 'title':'m(e, #tau)', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
     {'var':'bdt_evt_Mmt', 'varname':'bdt_evt_Mmt', 'legend':'top right', 'logy':False, 'title':'m(#mu, #tau)', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
-    # {'var':'bdt_evt_LT', 'varname':'bdt_evt_LT', 'legend':'top right', 'logy':False, 'title':'#Sigma lepton p_{T}', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':300.},
-    # {'var':'bdt_evt_L2T', 'varname':'bdt_evt_L2T', 'legend':'top right', 'logy':False, 'title':'m_{vis}(#tau, l_{2})', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':200.},
+    {'var':'bdt_evt_Mem', 'varname':'bdt_evt_Mem', 'legend':'top right', 'logy':False, 'title':'m(e, #mu)', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
+    {'var':'bdt_evt_LT', 'varname':'bdt_evt_LT', 'legend':'top right', 'logy':False, 'title':'#Sigma lepton p_{T}', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':300.},
+    {'var':'bdt_evt_L2T', 'varname':'bdt_evt_L2T', 'legend':'top right', 'logy':False, 'title':'m_{vis}(#tau, l_{2})', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':200.},
     {'var':'bdt_evt_nbjet', 'varname':'bdt_evt_nbjet', 'legend':'top right', 'logy':False, 'title':'N_{b jet}', 'unit':'GeV', 'nbinsx':5, 'xmin':-0.5, 'xmax':4.5},
+    {'var':'bdt_evt_nbjet10', 'varname':'bdt_evt_nbjet10', 'legend':'top right', 'logy':False, 'title':'N_{b jet} (10 GeV)', 'unit':'GeV', 'nbinsx':5, 'xmin':-0.5, 'xmax':4.5},
     {'var':'bdt_evt_HT', 'varname':'bdt_evt_HT', 'legend':'top right', 'logy':False, 'title':'#Sigma p_{T} (jets, leptons)', 'unit':'GeV', 'nbinsx':10, 'xmin':100., 'xmax':600.},
     {'var':'abs(bdt_evt_deltaeta)', 'varname':'abs_bdt_evt_deltaeta', 'legend':'top right', 'logy':False, 'title':'|#Delta #eta(forward jet, muon)|', 'unit':'GeV', 'nbinsx':5, 'xmin':0., 'xmax':5.},
     {'var':'abs(bdt_muon_eta-bdt_tau_eta)', 'varname':'abs_bdt_muon_eta_MINUS_bdt_tau_eta', 'legend':'top right', 'logy':False, 'title':'|#Delta #eta(#mu, #tau)|', 'unit':'GeV', 'nbinsx':5, 'xmin':0., 'xmax':5.},
+    {'var':'abs(bdt_muon_eta-bdt_smuon_eta)', 'varname':'abs_bdt_muon_eta_MINUS_bdt_smuon_eta', 'legend':'top right', 'logy':False, 'title':'|#Delta #eta(#mu, e)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':5.},
+    {'var':'abs(bdt_muon_eta-bdt_tau_eta)', 'varname':'abs_bdt_muon_eta_MINUS_bdt_tau_eta', 'legend':'top right', 'logy':False, 'title':'|#Delta #eta(#mu, #tau)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':5.},
+    {'var':'abs(bdt_smuon_eta-bdt_tau_eta)', 'varname':'bdt_smuon_eta', 'legend':'top right', 'logy':False, 'title':'|#Delta #eta(e, #tau)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':5.},
     {'var':'bdt_evt_maxMT', 'varname':'bdt_evt_maxMT', 'legend':'top right', 'logy':False, 'title':'max(M_{T} #mu, e)', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
     {'var':'bdt_tau_MT', 'varname':'bdt_tau_MT', 'legend':'top right', 'logy':False, 'title':'M_{T} #tau', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
+    {'var':'bdt_smuon_MT', 'varname':'bdt_smuon_MT', 'legend':'top right', 'logy':False, 'title':'M_{T} e', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
     {'var':'bdt_muon_MT', 'varname':'bdt_muon_MT', 'legend':'top right', 'logy':False, 'title':'M_{T} #mu', 'unit':'GeV', 'nbinsx':10, 'xmin':0., 'xmax':150.},
-    # {'var':'abs(bdt_evt_dphi_mete)', 'varname':'abs_bdt_evt_dphi_mete', 'legend':'top right', 'logy':False, 'title':'|#Delta #phi(E_{T}^{miss}, e)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':3.1415927},
+    {'var':'abs(bdt_evt_dphi_mete)', 'varname':'abs_bdt_evt_dphi_mete', 'legend':'top right', 'logy':False, 'title':'|#Delta #phi(E_{T}^{miss}, e)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':3.1415927},
     {'var':'abs(bdt_evt_dphi_metmu)', 'varname':'abs_bdt_evt_dphi_metmu', 'legend':'top right', 'logy':False, 'title':'|#Delta #phi(E_{T}^{miss}, #mu)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':3.1415927},
     {'var':'abs(bdt_evt_dphi_mettau)', 'varname':'abs_bdt_evt_dphi_mettau', 'legend':'top right', 'logy':False, 'title':'|#Delta #phi(E_{T}^{miss}, #tau)|', 'unit':'', 'nbinsx':5, 'xmin':0., 'xmax':3.1415927},
+    {'var':'bdt_muon_jet_csv', 'varname':'bdt_muon_jet_csv_10', 'legend':'top right', 'logy':False, 'title':'CSV #mu jet (10 GeV)', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.00001},
+    {'var':'bdt_muon_jet_csv_10', 'varname':'bdt_muon_jet_csv', 'legend':'top right', 'logy':False, 'title':'CSV #mu jet', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.00001},
+    {'var':'bdt_tau_jet_csv', 'varname':'bdt_tau_jet_csv', 'legend':'top right', 'logy':False, 'title':'CSV #tau jet', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.00001},
+    {'var':'bdt_smuon_jet_csv', 'varname':'bdt_smuon_jet_csv', 'legend':'top right', 'logy':False, 'title':'CSV e jet', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.00001},
+    {'var':'bdt_smuon_jet_csv', 'varname':'bdt_smuon_jet_csv_10', 'legend':'top right', 'logy':False, 'title':'CSV e jet (10 GeV)', 'unit':'', 'nbinsx':10, 'xmin':0., 'xmax':1.00001},
+    {'var':'abs(bdt_muon_pdg)', 'varname':'bdt_muon_pdg', 'legend':'top right', 'logy':False, 'title':'PDG muon match', 'unit':'', 'nbinsx':16, 'xmin':-0.5, 'xmax':15.5},
+    {'var':'abs(bdt_smuon_pdg)', 'varname':'bdt_smuon_pdg', 'legend':'top right', 'logy':False, 'title':'PDG smuon match', 'unit':'', 'nbinsx':16, 'xmin':-0.5, 'xmax':15.5},
+    {'var':'abs(bdt_tau_pdg)', 'varname':'bdt_tau_pdg', 'legend':'top right', 'logy':False, 'title':'PDG tau match', 'unit':'', 'nbinsx':16, 'xmin':-0.5, 'xmax':15.5},
+    {'var':'abs(bdt_evt_dr_mujet)', 'varname':'bdt_evt_dr_mujet', 'legend':'top right', 'logy':False, 'title':'#Delta R(#mu, jet)', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':6.},
+    {'var':'abs(bdt_evt_dr_taujet)', 'varname':'bdt_evt_dr_taujet', 'legend':'top right', 'logy':False, 'title':'#Delta R(#tau, jet)', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':6.},
+    {'var':'abs(bdt_evt_dr_ejet)', 'varname':'bdt_evt_dr_ejet', 'legend':'top right', 'logy':False, 'title':'#Delta R(e, jet)', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':6.},
+    {'var':'bdt_evt_dr_mujet_csv', 'varname':'bdt_evt_dr_mujet_csv', 'legend':'top right', 'logy':False, 'title':'CSV jet near #mu', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':1.0001},
+    {'var':'bdt_evt_dr_ejet_csv', 'varname':'bdt_evt_dr_ejet_csv', 'legend':'top right', 'logy':False, 'title':'CSV jet near e', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':1.0001},
+    {'var':'bdt_evt_dr_taujet_csv', 'varname':'bdt_evt_dr_taujet_csv', 'legend':'top right', 'logy':False, 'title':'CSV jet near #tau', 'unit':'', 'nbinsx':15, 'xmin':0., 'xmax':1.0001},
+    {'var':'log(abs(bdt_muon_dB3D))', 'varname':'bdt_muon_dB3D', 'legend':'top right', 'logy':False, 'title':'#mu log(|dB_{3D}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_muon_dz))', 'varname':'bdt_muon_dz', 'legend':'top right', 'logy':False, 'title':'#mu log(|d_{z}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_muon_dxy))', 'varname':'bdt_muon_dxy', 'legend':'top right', 'logy':False, 'title':'#mu log(|d_{xy}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_smuon_dB3D))', 'varname':'bdt_smuon_dB3D', 'legend':'top right', 'logy':False, 'title':'e log(|dB_{3D}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_smuon_dz))', 'varname':'bdt_smuon_dz', 'legend':'top right', 'logy':False, 'title':'e log(|d_{z}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_smuon_dxy))', 'varname':'bdt_smuon_dxy', 'legend':'top right', 'logy':False, 'title':'e log(|d_{xy}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_tau_dB3D))', 'varname':'bdt_tau_dB3D', 'legend':'top right', 'logy':False, 'title':'tau log(|dB_{3D}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_tau_dz))', 'varname':'bdt_tau_dz', 'legend':'top right', 'logy':False, 'title':'tau log(|d_{z}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+    {'var':'log(abs(bdt_tau_dxy))', 'varname':'bdt_tau_dxy', 'legend':'top right', 'logy':False, 'title':'tau log(|d_{xy}|)', 'unit':'log(cm)', 'nbinsx':15, 'xmin':-10., 'xmax':0.},
+
     ]
 
+    sampleDict = {
+        1:{'name':'WW', 'colour':ROOT.TColor.GetColor(222,90,106), 'label':'Diboson'},
+        1:{'name':'WZ', 'colour':ROOT.TColor.GetColor(222,90,106), 'label':'Diboson'},
+        2:{'name':'ZZ', 'colour':ROOT.TColor.GetColor(222,90,106), 'label':'Diboson'},
+        3:{'name':'tt0l', 'colour':ROOT.TColor.GetColor(155,133,296), 'label':'t#bar{t}'},
+        4:{'name':'tt1l', 'colour':ROOT.TColor.GetColor(155,152,296), 'label':'t#bar{t}'},
+        5:{'name':'tt2l', 'colour':ROOT.TColor.GetColor(155,152,296), 'label':'t#bar{t}'},
+        # 4:{'name':'tt1l', 'colour':ROOT.TColor.GetColor(155,152,250), 'label':'t#bar{t}'},
+        # 5:{'name':'tt2l', 'colour':ROOT.TColor.GetColor(155,152,204), 'label':'t#bar{t}'},
 
-    p = Plotter(variables, sampleDict, fileName="BDT_training_mva.root", signalsampleIds=[13], datasampleIds=[100])
+        # # 6:{'name':'DY', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'DY'},
+        # 7:{'name':'DY1', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'DY'},
+        # 8:{'name':'DY2', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'DY'},
+        # 9:{'name':'DY3', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'DY'},
+        # 10:{'name':'DY4', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'DY'},
+
+        # # 11:{'name':'Wjet', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'W'},
+        # 12:{'name':'W1jet', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'W'},
+        # 13:{'name':'W2jet', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'W'},
+        # 14:{'name':'W3jet', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'W'},
+        # 15:{'name':'W4jet', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'W'},
+        17:{'name':'ttW', 'colour':ROOT.TColor.GetColor(248,206,104), 'label':'t#bar{t}W/Z'},
+        18:{'name':'ttZ', 'colour':ROOT.TColor.GetColor(248,206,104), 'label':'t#bar{t}W/Z'}, #'label':'t#bar{t}Z'
+        16:{'name':'tH_YtMinus', 'colour':ROOT.TColor.kRed + 3, 'label':'tH (y_{t}=-1)'},
+        19:{'name':'ttH', 'colour':ROOT.TColor.GetColor(100,182,232), 'label':'t#bar{t}H'},
+        20:{'name':'reducible', 'colour':ROOT.TColor.GetColor(250,202,255), 'label':'Reducible'},
+        # 100:{'name':'data', 'colour':1, 'label':'Data'}, # FIXME: 100?
+    }
+    p = Plotter(variables, sampleDict, fileName="BDT_training_ss_f12_nottbar_mva.root", directory='plots',
+        signalsampleIds=[16], datasampleIds=[100])
     p.readTuples()
     p.makePlots(weight='bdt_evt_weight')
     
--- mumutau/tmva_cat_training.py
+++ mumutau/tmva_cat_training.py
@@ -1,34 +1,45 @@
-import numpy
+import optparse
 
 import ROOT
-TMVA_tools = ROOT.TMVA.Tools.Instance()
 
 
+training_vars = ['bdt_evt_leading_btag_pt','bdt_evt_leading_btag', 'bdt_evt_max_jet_eta', '(bdt_evt_max_jet_eta30*(bdt_evt_max_jet_eta30>0.))', 'bdt_evt_njet_or30', 'bdt_evt_Msmt', 'bdt_evt_Mmt', 'bdt_evt_Mmm',  'bdt_evt_missing_et', 'bdt_evt_nbjet', 'bdt_evt_centrality', 'bdt_muon_jet_csv', 'bdt_muon_charge+bdt_smuon_charge+bdt_tau_charge']#'bdt_evt_LT', , 
+# 'bdt_evt_HT', 'abs(bdt_evt_deltaeta)', 'abs(bdt_muon_eta-bdt_tau_eta)', 'abs(bdt_muon_eta-bdt_smuon_eta)', 'abs(bdt_smuon_eta-bdt_tau_eta)', 'bdt_evt_maxMT', 'abs(bdt_evt_dphi_mete)', 'abs(bdt_evt_dphi_metmu)', 'abs(bdt_evt_dphi_mettau)']#, 'bdt_tau_decaymode', 'bdt_muon_charge',
 
+# training_vars = ['bdt_evt_max_jet_eta','bdt_evt_njet_or30', 'bdt_evt_sphericity', 'bdt_evt_aplanarity']
+# training_vars = ['bdt_evt_max_jet_eta','bdt_evt_njet_or30', 'bdt_evt_sphericity', 'bdt_evt_aplanarity', 'log(abs(bdt_muon_dz))', 'bdt_evt_sleading_btag_pt', 'bdt_muon_jet_csv']
 
-training_vars = ['bdt_evt_leading_btag_pt', 'bdt_evt_leading_btag', 'bdt_evt_sleading_btag', 'bdt_tau_decaymode', 'bdt_stau_decaymode', 'bdt_evt_missing_et', 'bdt_evt_nvetobjet', 'bdt_evt_max_jet_eta30']#, 'bdt_evt_max_jet_eta', 'bdt_evt_njet_or', 'bdt_evt_njet', 'bdt_evt_centrality', 'bdt_evt_H', 'bdt_evt_HT', 'bdt_evt_sumjetpt', 'bdt_evt_Mmt', 'bdt_evt_sMmt',
-    # 'bdt_evt_dphi_metmu',  'bdt_evt_dphi_mettau', 'bdt_evt_dphi_metstau', 'bdt_stau_MT', 'bdt_tau_MT', 'bdt_muon_MT']
+# training_vars = ['bdt_evt_nbjet', 'bdt_evt_centrality', 'bdt_muon_charge', 'bdt_evt_HT']
 
 # training_vars = ['bdt_evt_centrality', 'abs(bdt_evt_dphi_metmu)', 'bdt_evt_maxMT', 'bdt_evt_missing_et']
+
 obs_vars = []
 
-
 basic_selection = ''
-signal_selection = '(bdt_evt_isSignal > 0.5 && bdt_evt_processid==13)*(bdt_evt_njet_or30>1)' #tH -1
+signal_selection = '(bdt_evt_isSignal > 0.5 && bdt_evt_processid==16)' #tH -1
 # signal_selection = '(bdt_evt_processid==7)' #ttH
-background_selection = '(bdt_evt_isSignal < 0.5 && bdt_evt_processid < 50 && (!{signal}))*(bdt_evt_njet_or30>1)'.format(signal=signal_selection) # no data!
+# background_selection = '(bdt_evt_isSignal < 0.5 && bdt_evt_processid != 20 && bdt_evt_processid < 50 && (!{signal}))'.format(signal=signal_selection) # no data!
+background_selection = '(bdt_evt_isSignal < 0.5 && bdt_evt_processid < 50 && (!{signal}))'.format(signal=signal_selection)
 
 if '7' in signal_selection:
     signal_selection += '*(bdt_evt_njet_or30>1)'
     background_selection += '*(bdt_evt_njet_or30>1)'
 
-    
+def parse_options():
+    usage = '''
+%prog [options]
+'''
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-i', '--input', dest='input_file', help='input file name', default='/afs/cern.ch/user/y/ytakahas/public/forJan/tH_BDTtraining_20141009/BDT_training_ss_f3.root', type='string')
+    parser.add_option('-o', '--out_postfix', dest='out_postfix', help='output file postfix', default='', type='string')
+    opts, args = parser.parse_args()
+    return opts, args
 
+def train(fileName, postfix):
+    TMVA_tools = ROOT.TMVA.Tools.Instance()
 
+    tfile = ROOT.TFile(fileName, postfix)
 
-
-def train():
-    tfile = ROOT.TFile('BDT_training.root')
     tree = tfile.Get('Tree')
 
     num_pass = tree.GetEntries(signal_selection)
@@ -46,7 +57,9 @@ def train():
 
     print 'N events signal', num_pass
     print 'N events background', num_fail
-    outFile = ROOT.TFile('TMVA_classification.root', 'RECREATE')
+
+
+    outFile = ROOT.TFile('TMVA_classification{postfix}.root'.format(postfix=postfix), 'RECREATE')
 
     factory    = ROOT.TMVA.Factory(
         "TMVAClassification", 
@@ -66,20 +79,21 @@ def train():
     factory.PrepareTrainingAndTestTree( ROOT.TCut(signal_selection), ROOT.TCut(background_selection),
                                         "nTrain_Signal=0:nTest_Background=0:SplitMode=Random:NormMode=NumEvents:!V" )
 
-
     # factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDTG","!H:!V:NTrees=250::BoostType=Grad:Shrinkage=0.05:UseBaggedBoost:GradBaggingFraction=0.9:nCuts=500:MaxDepth=4:MinNodeSize=5" )
 
     # Optimized:
     # factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDTG","!H:!V:NTrees=250::BoostType=Grad:Shrinkage=0.275:UseBaggedBoost:GradBaggingFraction=0.9:nCuts=500:MaxDepth=2:MinNodeSize=15" )
     # Re-Optimized:
-    factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDTG","!H:!V:NTrees=1000::BoostType=Grad:Shrinkage=0.5:UseBaggedBoost:GradBaggingFraction=0.9:nCuts=500:MaxDepth=2:MinNodeSize=1" )
+    # factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDTG","!H:!V:NTrees=1000::BoostType=Grad:Shrinkage=0.5:UseBaggedBoost:GradBaggingFraction=0.9:nCuts=500:MaxDepth=2:MinNodeSize=1" )
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDTG"+postfix, "!H:!V:NTrees=500::BoostType=Grad:Shrinkage=0.05:UseBaggedBoost:GradBaggingFraction=0.9:nCuts=500:MaxDepth=4:MinNodeSize=10" )
 
 
     # factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT_TOP", "!H:!V:NTrees=400:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=50:AdaBoostBeta=0.005:MaxDepth=5")
     # Optimizerd:
-    factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT_TOP", "!H:!V:NTrees=400:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=50:AdaBoostBeta=0.2:MaxDepth=2:MinNodeSize=6")
+    factory.BookMethod(ROOT.TMVA.Types.kBDT, "BDT_TOP"+postfix, "!H:!V:NTrees=400:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=50:AdaBoostBeta=0.2:MaxDepth=2:MinNodeSize=6")
 
-    factory.BookMethod( ROOT.TMVA.Types.kFisher, "Fisher", "H:!V:Fisher:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10" )
+    factory.BookMethod( ROOT.TMVA.Types.kFisher, "Fisher"+postfix, "H:!V:Fisher:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10" )
+    # factory.BookMethod( ROOT.TMVA.Types.kFisher, "Fisher2"+postfix, "H:!V:Mahalanobis" )
 
     # factory.BookMethod( ROOT.TMVA.Types.kSVM, "SVM", "Gamma=0.25:Tol=0.001:VarTransform=Norm" )
 # --- OptimizeConfigParamete...: For BDT_TOP the optimized Parameters are: 
@@ -92,6 +106,12 @@ def train():
 # --- OptimizeConfigParamete...: MinNodeSize = 1
 # --- OptimizeConfigParamete...: NTrees = 1000
 # --- OptimizeConfigParamete...: Shrinkage = 0.5
+
+# --- OptimizeConfigParamete...: For BDTG the optimized Parameters are: 
+# --- OptimizeConfigParamete...: MaxDepth = 4
+# --- OptimizeConfigParamete...: MinNodeSize = 10
+# --- OptimizeConfigParamete...: NTrees = 505
+# --- OptimizeConfigParamete...: Shrinkage = 0.05
 
 # --- <WARNING> BDT_TOP                  : AdaBoostBeta = 0.2
 # --- <WARNING> BDT_TOP                  : MaxDepth = 2
@@ -111,9 +131,9 @@ def train():
     # ROOT.TMVARegGui('TMVA.root')
 
 
-
-
 if __name__ == '__main__':
-    train()
-    # read()
+    opts, args = parse_options()
 
+    input_file = opts.input_file
+    postfix = opts.out_postfix
+    train(input_file, postfix)
